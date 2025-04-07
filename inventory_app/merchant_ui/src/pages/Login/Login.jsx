4b4cc04af4100852400d685c48fe7f42f8922cf2
Abishek A <141331319+007abishek@users.noreply.github.com>
2025-04-07 23:12:39 +0530

    Update Login.jsx

--- inventory_app/merchant_ui/src/pages/Login/Login.jsx
+++ inventory_app/merchant_ui/src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './login.css';
+import './Login.css';
 
 const MerchantLogin = () => {
   const [formData, setFormData] = useState({ email: '', password: '' });
