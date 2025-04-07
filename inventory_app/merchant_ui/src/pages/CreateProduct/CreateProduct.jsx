c0bcb259c167bc06e6a68bd877a6272dad2751ee
Abishek A <141331319+007abishek@users.noreply.github.com>
2025-04-07 22:49:05 +0530

    Update CreateProduct.jsx

--- inventory_app/merchant_ui/src/pages/CreateProduct/CreateProduct.jsx
+++ inventory_app/merchant_ui/src/pages/CreateProduct/CreateProduct.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import './CreateProduct.css';
 
 const api = axios.create({
-  baseURL: "http://localhost:5001", // Update with deployed backend URL if needed
+  baseURL: "https://inventory-backend-buxd.onrender.com", // Update with deployed backend URL if needed
   withCredentials: true,
 });
 
